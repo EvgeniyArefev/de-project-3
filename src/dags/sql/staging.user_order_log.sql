e6cd7dbc4f533299ac52a22de0ffe4449bae6fd8
Evgeniy Arefev <73953829+EvgeniyArefev@users.noreply.github.com>
2022-12-27 07:50:01 +0300

    Delete staging.user_order_log.sql

--- src/dags/sql/staging.user_order_log.sql
+++
@@ -1,4 +0,0 @@
-delete from staging.user_order_log
-where date_time::date = ('{{ds}}'::date - {{ params.date_delta }});
-
-alter table staging.user_order_log add column if not exists status varchar(30);
